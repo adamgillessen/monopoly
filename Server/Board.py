e00d37105065a78e7c90345e385310b8c5ac6558
Adam Gillessen <adamgillessen@gmail.com>
2017-02-13 01:07:31 +0000

    Added board functions and acompanying unit tests

--- Server/Board.py
+++ Server/Board.py
@@ -12,7 +12,8 @@ class Board:
     CHEST_POS = [2, 17, 33]
     CHANCE_POS = [7, 22, 36]
     CORNER_POS = [0, 10, 20, 30]
-    # PROPERTY_POS_INFO[position] = [price, rent, estate]
+    TAX_POS = [4, 38]
+    # PROPERTY_POS_INFO[position] = [price, rent, estate, id]
     PROPERTY_POS_INFO = {
         1 : [60, 10, 0, 0],
         3 : [60, 20, 0, 1],
@@ -36,7 +37,8 @@ class Board:
         34 : [320, 150, 6, 19],
         37 : [350, 175, 7, 20],
         39 : [400, 200, 7, 21],
-    } 
+    }
+    JAIL_POS = 9
     
     def __init__(self, num_players):
         """
@@ -44,35 +46,134 @@ class Board:
         """
 
         # Initialise Squares
-        self._b = [None for _ in range(Board.NUM_SQUARES)]
+        self._board = [None for _ in range(Board.NUM_SQUARES)]
 
         for pos in Board.UTIL_POS:
-            self._b[pos] = UtilitySquare(pos)
+            self._board[pos] = UtilitySquare(pos)
 
         for pos in Board.TRANS_POS:
-            self._b[pos] = TransportSquare(pos)
+            self._board[pos] = TransportSquare(pos)
 
         for pos in Board.CHEST_POS:
-            self._b[pos] = ActionSquare(pos, action="chest")
+            self._board[pos] = ActionSquare(pos, action="chest")
 
         for pos in Board.CHANCE_POS:
-            self._b[pos] = ActionSquare(pos, action="chance")
+            self._board[pos] = ActionSquare(pos, action="chance")
+
+        for pos in Board.TAX_POS:
+            self._board[pos] = ActionSquare(pos, action="tax")
 
         for pos in Board.CORNER_POS:
             if pos == Board.CORNER_POS[1] or pos == Board.CORNER_POS[2]:
-                self._b[pos] = ActionSquare(pos, action="stay")
+                self._board[pos] = ActionSquare(pos, action="stay")
             elif pos == Board.CORNER_POS[0]:
-                self._b[pos] = ActionSquare(pos, action="go")
+                self._board[pos] = ActionSquare(pos, action="go")
             elif pos == Board.CORNER_POS[3]:
-                self._b[pos] = ActionSquare(pos, action="jail")
+                self._board[pos] = ActionSquare(pos, action="jail")
 
         for pos, info in Board.PROPERTY_POS_INFO.items():
             price, rent, estate, prop_id = info 
-            self._b[pos] = PropertySquare(pos, price, rent, estate, prop_id)
+            self._board[pos] = PropertySquare(pos, price, rent, estate, prop_id)
 
-        # Initialise players
-        for _ in range(num_players):
-            self._b[0].add_player(Player())
+        # create players
+        self._players = [Player(i) for i in range(num_players)]
 
-if __name__ == "__main__":
-    Board(4)
+        # Initialise players at position 0 (Go)
+        self._player_positions = {}
+        for p in self._players:
+            self._player_positions[p] = 0 
+            self._board[0].add_player(p)
+
+    def __str__(self):
+        """
+        Returns a string representation of the baord. 
+        """
+        s = ""
+        for i, square in enumerate(self._board):
+            if i > 0 and not i%5:
+                s += "\n"
+            s += str(square) + " "
+        return s 
+
+
+    def move_player(self, player_id, new_pos):
+        """
+        Moves Player with id "player_id" from old position to "new_pos". 
+        """
+        player = self._players[player_id]
+        # remove from old square
+        self._board[self._player_positions[player]].remove_player(player)
+
+        # add to new square
+        self._player_positions[player] = new_pos
+        self._board[new_pos].add_player(player)
+
+    def get_players(self, pos):
+        """
+        returns a list of all players at position pos. 
+        """
+        return list(self._board[pos])
+
+    def get_pos(self, player_id):
+        """
+        Returns the square id which the player with id "player_id" is on. 
+        """
+        return self._player_positions[self._players[player_id]]
+
+    def get_square(self, pos):
+        """
+        Returns the square at position pos. 
+        """
+        if pos >= Board.NUM_SQUARES:
+            raise IndexError(
+                "Board.get_square({}): Index {} not in Board of size {}".format(
+                    pos, pos, Board.NUM_SQUARES)) 
+        return self._board[pos]
+
+    def take_money(self, player_id, amount):
+        """
+        Takes "amount" amount of money from Player with id "player_id"
+        """
+        player = self._players[player_id]
+        player.money -= amount
+
+    def give_money(self, player_id, amount):
+        """
+        Gives "amount" amount of money to Player with id "player_id"
+        """
+        player = self._players[player_id]
+        player.money += amount
+
+    def obtain_get_out_jail_free(self, player_id):
+        """
+        Gives the Player with id "player_id" a get out of jail free card. 
+        """
+        player = self._players[player_id]
+        player.free = True 
+
+    def go_to_jail(self, player_id):
+        """
+        Moves player with id "player_id" to jail"
+        """
+        self.move_player(player_id, Board.JAIL_POS)
+        player = self._players[player_id]
+        player.jail = True 
+
+    def leave_jail(self, player_id):
+        """
+        Moves the player wit id "player_id" out of jail
+        """
+        player = self._players[player_id]
+        player.jail = False 
+
+    def use_get_out_jail_free(self, player_id):
+        """
+        Player with id "player_id" uses their get out of jail free card. 
+        """
+        player = self._players[player_id]
+        if not player.free:
+            raise ValueError(
+                "Board.use_get_out_jail_free({}): Player with id {} has no get out of jail free card".format(
+                    player_id, player_id))
+        player.free = False 
+        player.jail = False 
--- Server/Player.py
+++ Server/Player.py
@@ -2,10 +2,9 @@ class Player:
     """
     This class represents a player's state. 
     """
-    ID = 0
     INITIAL_MONEY = 2000
 
-    def __init__(self):
+    def __init__(self, player_id):
         """
         id - player id
         money - the amount of money the player currently has
@@ -15,11 +14,48 @@ class Player:
         transports - the transports which the player currently owns 
         free - True if the player has a get out if jail free card, else False
         """
-        self._id = Player.ID 
-        Player.ID += 1 
+        self._id = player_id
         self._money = Player.INITIAL_MONEY
         self._properties = set()
         self._position = 0
         self._utils = set()
         self._transports = set() 
         self._free = False 
+        self._jail = False 
+
+    def __str__(self):
+        """
+        Returns a string represention of the player. 
+        """
+        return "Player{}".format(self._id)
+
+    @property
+    def money(self):
+        return self._money
+
+    @money.setter
+    def money(self, new_amount):
+        self._money = new_amount
+
+    @property
+    def free(self):
+        return self._free 
+
+    @free.setter
+    def free(self, new_val):
+        self._free = new_val
+
+    @property
+    def jail(self):
+        return self._jail
+
+    @jail.setter
+    def jail(self, new_val):
+        self._jail = new_val
+
+    def get_id(self):
+        """
+        Returns the id of self. 
+        """
+        return self._id 
+
