a6d4c5eb21eada0a6fae540d4d656794e4848f8d
Adam Gillessen <adamgillessen@gmail.com>
2017-02-13 21:12:34 +0000

    Fixed player join and starting game functionality

--- Server/Server.py
+++ Server/Server.py
@@ -5,32 +5,36 @@ import json
 Run to install the websocket_server:
     sudo pip3 install websocket-server
 """
-
+s = None
 
 class Server:
     """
     This class holds all Server information. 
     """
-    CLIENT_ID = 1
 
     def __init__(self):
-        self.client_keys = {}
+        self._next_roll = 1
+        self._board = None
+        self._num_players = 0
 
     def next_id(self):
         """
         Returns the next client id. 
         """
-        temp = Server.CLIENT_ID
-        Server.CLIENT_ID += 1 
-        return temp 
+        self._num_players += 1
+        return self._num_players 
 
     def num_players(self):
         """
         Returns the number of players currently connected to the game. 
         """
-        return len(self.client_keys)
+        return self._num_players
 
-s = Server()
+    def start_game(self):
+        """
+        Initialises a board with the current players. 
+        """
+        self._b = Board(self.num_players())
 
 def new_client(client, server):
     """
@@ -50,27 +54,38 @@ def recv_message(client, server, message):
     server - a reference to the WebSocket Server
     message - the message which has been received
     """
-    global s
+    global s 
     print("Received: {}".format(message))
     json_string = json.loads(message)
 
     if json_string["type"] == "player_join":
-        s.client_keys[client["id"]] = json_string["key"]
-
         response_json = {
             "type" : "player_join_ack",
             "key" : json_string["key"],
             "your_id" : s.next_id(),
             "current_player" : s.num_players(),
             "expects" : 4,
-            "game_start" : True if s.num_players() == 4 else False,
+            "game_start" : False if s.num_players() < 4 else True,
         }
-
         response_json_string = json.dumps(response_json)
         server.send_message_to_all(response_json_string.encode("utf-8"))
 
+    elif json_string["type"] == "start_game_now":
+        response_json = {
+            "type": "player_join_ack",
+            "key" : -1,
+            "your_id": -1,
+            "current_player" : s.num_players(),
+            "expects" : 4,
+            "game_start" : True,
 
-ws = websocket_server.WebsocketServer(4444, "127.0.0.1")
-ws.set_fn_new_client(new_client)
-ws.set_fn_message_received(recv_message)
-ws.run_forever()
+        }
+        server.send_message_to_all(response_json_string.encode("utf-8"))
+
+
+if __name__ == "__main__":
+    s = Server()
+    ws = websocket_server.WebsocketServer(4444, "127.0.0.1")
+    ws.set_fn_new_client(new_client)
+    ws.set_fn_message_received(recv_message)
+    ws.run_forever()
