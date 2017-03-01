053e46b886987e07bc2cc25cdb1fd850ab937765
Adam Gillessen <adamgillessen@gmail.com>
2017-03-01 15:34:51 +0000

    Cahnged locaton of client storage to a live location

--- Client/copy_to_host.sh
+++ Client/copy_to_host.sh
@@ -1,14 +1,18 @@
-pi="pi@adamgillessen.ddns.net:/var/www/html/"
+pi_client="pi@adamgillessen.ddns.net:/var/www/html/"
 pi_server="pi@adamgillessen.ddns.net:/home/pi/monopoly_server/"
-netsoc="gilly@leela.netsoc.co:/home/gilly/public_html/"
+netsoc_client="gilly@leela.netsoc.co:/media/thor/home/gilly/public_html/"
+netsoc_server="gilly@leela.netsoc.co:/home/gilly/monopoly_server"
 
-scp ./index.html "$pi"index.html
-scp -r ./data "$pi"data
-scp -r ./images "$pi"images
-scp -r ./js "$pi"js
-scp -r ./lib "$pi"lib
-scp -r ./scss "$pi"scss
+client_location=$netsoc_client
+server_location=$netsoc_server
+
+
+scp ./index.html "$client_location"index.html
+scp -r ./data "$client_location"data
+scp -r ./images "$client_location"images
+scp -r ./js "$client_location"js
+scp -r ./lib "$client_location"lib
+scp -r ./scss "$client_location"scss
 
 cd ../Server/
-pwd
-scp *.py "$pi_server"
+scp *.py "$server_location"
